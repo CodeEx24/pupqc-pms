1547f6636b91214b0bf137a17e6d188231a5fe09
Jocarl Basco <76276243+jocarlbasco@users.noreply.github.com>
2023-04-19 14:56:52 +0800

    Dev 08: Fixed post

--- components/hooks/FacultySubject/addData.js
+++ components/hooks/FacultySubject/addData.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const addSubjectData = (data) => {
-  return axios.post(`http://localhost:3000/api/subject/insert`, data);
+  return axios.post(`/api/subject/insert`, data);
 };
 
 export const addSubjectClassData = (data) => {
-  return axios.post(`http://localhost:3000/api/class/insert`, data);
+  return axios.post(`/api/class/insert`, data);
 };
