e091822da10a7632fb9d411872766b0c820da16e
Jocarl Basco <76276243+jocarlbasco@users.noreply.github.com>
2023-04-26 01:12:43 +0800

    Dev 10: Test Deploy(Subject Class)

--- pages/faculty/student-list.js
+++ pages/faculty/student-list.js
@@ -2,6 +2,7 @@ import FacultyLayout from '@/components/faculty/FacultyLayout';
 import StudentList from '@/components/faculty/grid/StudentList';
 import { useQuery } from '@tanstack/react-query';
 import { fetchStudents } from '../../components/hooks/FacultySubject/fetch';
+import Loading from '../../components/Loading';
 
 function StudentsListScreen() {
   const { data: students, isLoading } = useQuery(['students'], fetchStudents);
@@ -12,7 +13,7 @@ function StudentsListScreen() {
         <h1 className="text-sky-400 font-bold text-3xl mb-5">Students Lists</h1>
         <div className="flex items-end gap-3">
           <div className="mb-6 w-full">
-            {isLoading ? 'Loading...' : <StudentList students={students} />}
+            {isLoading ? <Loading /> : <StudentList students={students} />}
           </div>
         </div>
       </div>
