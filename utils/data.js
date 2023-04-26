dba19b22c6966732e2a5f5631cfc97114b8f3980
Jocarl Basco <76276243+jocarlbasco@users.noreply.github.com>
2023-04-26 23:34:45 +0800

    Dev 10: Test Deploy v1

--- utils/data.js
+++ utils/data.js
@@ -14,6 +14,152 @@ import { FaMoneyBillAlt, FaUser } from 'react-icons/fa';
 
 import { TbReportAnalytics } from 'react-icons/tb';
 
+export const studentLinks = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'home',
+        icon: <AiFillHome />,
+      },
+      {
+        name: 'profile',
+        icon: <FaUser />,
+      },
+    ],
+  },
+
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'enrollment',
+        icon: <AiFillIdcard />,
+      },
+      {
+        name: 'tuition',
+        icon: <FaMoneyBillAlt />,
+      },
+      {
+        name: 'schedules',
+        icon: <AiFillCalendar />,
+      },
+      {
+        name: 'grades',
+        icon: <TbReportAnalytics />,
+      },
+      {
+        name: 'scores',
+        icon: <AiFillSnippets />,
+      },
+      {
+        name: 'forms',
+        icon: <AiFillDiff />,
+      },
+      {
+        name: 'hdf',
+        icon: <RiContactsLine />,
+      },
+    ],
+  },
+];
+
+export const teacherLinks = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'home',
+        href: 'home',
+        icon: <AiFillHome />,
+      },
+    ],
+  },
+
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'subject',
+        href: 'subject',
+        icon: <AiFillIdcard />,
+      },
+      {
+        name: 'class subject',
+        href: 'class-subject',
+        icon: <AiFillDiff />,
+      },
+      {
+        name: 'student list',
+        href: 'student-list',
+        icon: <AiFillDiff />,
+      },
+      {
+        name: 'criteria',
+        href: 'criteria',
+        icon: <AiFillIdcard />,
+      },
+      {
+        name: 'section',
+        href: 'section',
+        icon: <FaMoneyBillAlt />,
+      },
+      {
+        name: 'performance',
+        href: 'performance',
+        icon: <AiFillCalendar />,
+      },
+      {
+        name: 'attendance',
+        href: 'attendance',
+        icon: <TbReportAnalytics />,
+      },
+      {
+        name: 'students grade',
+        href: 'students-grade',
+        icon: <AiFillSnippets />,
+      },
+      {
+        name: 'profile',
+        href: 'profile',
+        icon: <FaUser />,
+      },
+
+      // {
+      //   name: 'hdf',
+      //   icon: <RiContactsLine />,
+      // },
+    ],
+  },
+];
+
+export const themeColors = [
+  {
+    name: 'blue-theme',
+    color: '#38bdf8',
+  },
+  {
+    name: 'green-theme',
+    color: '#4ade80',
+  },
+  {
+    name: 'purple-theme',
+    color: '#c084fc',
+  },
+  {
+    name: 'red-theme',
+    color: '#f87171',
+  },
+  {
+    name: 'indigo-theme',
+    color: '#818cf8',
+  },
+  {
+    name: 'orange-theme',
+    color: '#fb923c',
+  },
+];
+
 export const data = {
   students: [
     {
@@ -356,10 +502,27 @@ export const data = {
 
   classSubject: [
     {
-      _id: '609f42e6c2585b8a5f9ddc07',
+      _id: '609f42e6c2585b8a5f9ddc01',
       subject_id: 'ITCS-032',
       semester: 1,
       class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+
+    {
+      _id: '609f42e6c2585b8a5f9ddc02',
+      subject_id: 'ITCS-032',
+      semester: 3,
+      class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc11',
+      subject_id: 'DSAA-033',
+      semester: 2,
+      class_id: '60971b780eaf050004123457',
       teacher_id: '609e16f7c2585b8a5f9ddc51',
       criteria_id: '609e16f7c2585b8a5f9ddc24',
     },
@@ -367,6 +530,70 @@ export const data = {
       _id: '609f42e6c2585b8a5f9ddc12',
       subject_id: 'DSAA-033',
       semester: 1,
+      class_id: '60971b780eaf050004123457',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc03',
+      subject_id: 'ITCS-032',
+      semester: 2,
+      class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc13',
+      subject_id: 'DSAA-033',
+      semester: 3,
+      class_id: '60971b780eaf050004123457',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc04',
+      subject_id: 'ITCS-032',
+      semester: 1,
+      class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc14',
+      subject_id: 'DSAA-033',
+      semester: 2,
+      class_id: '60971b780eaf050004123457',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc05',
+      subject_id: 'ITCS-032',
+      semester: 3,
+      class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc15',
+      subject_id: 'DSAA-033',
+      semester: 1,
+      class_id: '60971b780eaf050004123457',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc06',
+      subject_id: 'ITCS-032',
+      semester: 2,
+      class_id: '60971b780eaf050004123456',
+      teacher_id: '609e16f7c2585b8a5f9ddc51',
+      criteria_id: '609e16f7c2585b8a5f9ddc24',
+    },
+    {
+      _id: '609f42e6c2585b8a5f9ddc16',
+      subject_id: 'DSAA-033',
+      semester: 3,
       class_id: '60971b780eaf050004123457',
       teacher_id: '609e16f7c2585b8a5f9ddc51',
       criteria_id: '609e16f7c2585b8a5f9ddc24',
@@ -405,151 +632,155 @@ export const data = {
       description:
         'This course covers the fundamental concepts and techniques of artificial intelligence, including search algorithms, machine learning, natural language processing, and robotics.',
     },
+    {
+      _id: 'DBMS-732',
+      name: 'Database Management Systems',
+      description:
+        'This course covers the design, implementation, and maintenance of database systems, including data models, relational algebra, SQL, and database security.',
+    },
+    {
+      _id: 'NWKS-321',
+      name: 'Computer Networks',
+      description:
+        'This course covers the fundamentals of computer networks, including network architecture, protocols, topologies, and network security.',
+    },
+    {
+      _id: 'SYSP-443',
+      name: 'System Programming',
+      description:
+        'This course covers the concepts and techniques of system programming, including process management, memory management, and system calls.',
+    },
+    {
+      _id: 'WEBD-615',
+      name: 'Web Development',
+      description:
+        'This course covers the principles and practices of web development, including HTML, CSS, JavaScript, and web frameworks.',
+    },
+    {
+      _id: 'DSAI-721',
+      name: 'Data Science and Analytics',
+      description:
+        'This course covers the concepts and techniques of data science and analytics, including data preprocessing, data visualization, and machine learning.',
+    },
+    {
+      _id: 'CMPN-133',
+      name: 'Computer Organization and Architecture',
+      description:
+        'This course covers the fundamentals of computer organization and architecture, including digital logic, assembly language, and computer arithmetic.',
+    },
+    {
+      _id: 'SYSC-621',
+      name: 'Real-Time and Embedded Systems',
+      description:
+        'This course covers the principles and practices of real-time and embedded systems, including scheduling algorithms, interrupts, and device drivers.',
+    },
+    {
+      _id: 'PRGR-833',
+      name: 'Programming Languages',
+      description:
+        'This course covers the fundamental concepts and paradigms of programming languages, including syntax, semantics, and type systems.',
+    },
+    {
+      _id: 'PRJC-411',
+      name: 'Software Project Management',
+      description:
+        'This course covers the principles and practices of software project management, including project planning, estimation, tracking, and risk management.',
+    },
+    {
+      _id: 'SYSC-541',
+      name: 'Digital Signal Processing',
+      description:
+        'This course covers the concepts and techniques of digital signal processing, including signal analysis, filtering, and transformation.',
+    },
+    {
+      _id: 'MLBD-652',
+      name: 'Machine Learning and Big Data',
+      description:
+        'This course covers the concepts and techniques of machine learning and big data, including classification, clustering, and data mining.',
+    },
+    {
+      _id: 'INFO-511',
+      name: 'Information Retrieval',
+      description:
+        'This course covers the principles and techniques of information retrieval, including search engines, indexing, and relevance ranking.',
+    },
+    {
+      _id: 'SECY-711',
+      name: 'Cybersecurity',
+      description:
+        'This course covers the principles and practices of cybersecurity, including threat analysis, network security, and cryptography.',
+    },
+    {
+      _id: 'CMPN-622',
+      name: 'Computer Graphics',
+      description:
+        'This course covers the fundamental concepts and techniques of computer graphics, including 2D and 3D graphics, rendering, and animation.',
+    },
+    {
+      _id: 'MLBD-611',
+      name: 'Data Mining',
+      description:
+        'This course covers the concepts and techniques of data mining, including classification, clustering, and association rule mining.',
+    },
+    {
+      _id: 'INFO-621',
+      name: 'Human-Computer Interaction',
+      description:
+        'This course covers the principles and practices of human-computer interaction, including user-centered design, usability testing, and interface design.',
+    },
+    {
+      _id: 'EMBS-742',
+      name: 'Biomedical Signal Processing',
+      description:
+        'This course covers the concepts and techniques of biomedical signal processing, including signal analysis, filtering, and feature extraction for applications in healthcare and medical research.',
+    },
+    {
+      _id: 'GAME-531',
+      name: 'Game Development',
+      description:
+        'This course covers the principles and practices of game development, including game design, graphics, physics, and artificial intelligence.',
+    },
+    {
+      _id: 'IOTC-651',
+      name: 'Internet of Things and Cloud Computing',
+      description:
+        'This course covers the concepts and techniques of Internet of Things (IoT) and cloud computing, including IoT architectures, sensor networks, cloud platforms, and IoT applications.',
+    },
+    {
+      _id: 'ROBT-721',
+      name: 'Robotics',
+      description:
+        'This course covers the principles and practices of robotics, including kinematics, dynamics, control, and applications in industrial, medical, and entertainment domains.',
+    },
+    {
+      _id: 'CSCI-781',
+      name: 'Computer Vision',
+      description:
+        'This course covers the concepts and techniques of computer vision, including image processing, pattern recognition, and machine learning for applications in robotics, autonomous systems, and visual analytics.',
+    },
+    {
+      _id: 'NLPN-731',
+      name: 'Natural Language Processing',
+      description:
+        'This course covers the concepts and techniques of natural language processing, including text analysis, language modeling, and sentiment analysis for applications in conversational agents, machine translation, and text analytics.',
+    },
+    {
+      _id: 'CLPS-411',
+      name: 'Cognitive Psychology',
+      description:
+        'This course covers the fundamental concepts and theories of cognitive psychology, including attention, perception, memory, and language processing.',
+    },
+    {
+      _id: 'VISN-621',
+      name: 'Visual Analytics',
+      description:
+        'This course covers the principles and techniques of visual analytics, including data visualization, exploratory data analysis, and interactive graphics.',
+    },
+    {
+      _id: 'PRGS-611',
+      name: 'Progressive Web Applications',
+      description:
+        'This course covers the concepts and techniques of progressive web applications, including web app architecture, offline functionality, push notifications, and service workers.',
+    },
   ],
 };
-
-export const studentLinks = [
-  {
-    title: 'Dashboard',
-    links: [
-      {
-        name: 'home',
-        icon: <AiFillHome />,
-      },
-      {
-        name: 'profile',
-        icon: <FaUser />,
-      },
-    ],
-  },
-
-  {
-    title: 'Pages',
-    links: [
-      {
-        name: 'enrollment',
-        icon: <AiFillIdcard />,
-      },
-      {
-        name: 'tuition',
-        icon: <FaMoneyBillAlt />,
-      },
-      {
-        name: 'schedules',
-        icon: <AiFillCalendar />,
-      },
-      {
-        name: 'grades',
-        icon: <TbReportAnalytics />,
-      },
-      {
-        name: 'scores',
-        icon: <AiFillSnippets />,
-      },
-      {
-        name: 'forms',
-        icon: <AiFillDiff />,
-      },
-      {
-        name: 'hdf',
-        icon: <RiContactsLine />,
-      },
-    ],
-  },
-];
-
-export const teacherLinks = [
-  {
-    title: 'Dashboard',
-    links: [
-      {
-        name: 'home',
-        href: 'home',
-        icon: <AiFillHome />,
-      },
-    ],
-  },
-
-  {
-    title: 'Pages',
-    links: [
-      {
-        name: 'subject',
-        href: 'subject',
-        icon: <AiFillIdcard />,
-      },
-      {
-        name: 'class subject',
-        href: 'class-subject',
-        icon: <AiFillDiff />,
-      },
-      {
-        name: 'student list',
-        href: 'student-list',
-        icon: <AiFillDiff />,
-      },
-      {
-        name: 'criteria',
-        href: 'criteria',
-        icon: <AiFillIdcard />,
-      },
-      {
-        name: 'section',
-        href: 'section',
-        icon: <FaMoneyBillAlt />,
-      },
-      {
-        name: 'performance',
-        href: 'performance',
-        icon: <AiFillCalendar />,
-      },
-      {
-        name: 'attendance',
-        href: 'attendance',
-        icon: <TbReportAnalytics />,
-      },
-      {
-        name: 'students grade',
-        href: 'students-grade',
-        icon: <AiFillSnippets />,
-      },
-      {
-        name: 'profile',
-        href: 'profile',
-        icon: <FaUser />,
-      },
-
-      // {
-      //   name: 'hdf',
-      //   icon: <RiContactsLine />,
-      // },
-    ],
-  },
-];
-
-export const themeColors = [
-  {
-    name: 'blue-theme',
-    color: '#38bdf8',
-  },
-  {
-    name: 'green-theme',
-    color: '#4ade80',
-  },
-  {
-    name: 'purple-theme',
-    color: '#c084fc',
-  },
-  {
-    name: 'red-theme',
-    color: '#f87171',
-  },
-  {
-    name: 'indigo-theme',
-    color: '#818cf8',
-  },
-  {
-    name: 'orange-theme',
-    color: '#fb923c',
-  },
-];
