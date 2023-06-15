e14012d85ba59fc31b9f0ff1588ff22b6b33cdf2
Jocarl Basco <jocarlbasco24@gmail.com>
2023-06-15 21:16:58 +0800

    Dev 26: Fixing

--- pages/api/faculty/subject/passed-failed/[year].js
+++ pages/api/faculty/subject/passed-failed/[year].js
@@ -1,88 +1,88 @@
-// /api/subject - USED
+// // /api/subject - USED
 
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
+// import { getServerSession } from 'next-auth/next';
+// import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-import db from '@/utils/db';
-import ClassSubject from '@/models/ClassSubject';
-import Class from '../../../../../models/Class';
-import AverageClassGrade from '../../../../../models/AverageClassGrade';
+// import db from '@/utils/db';
+// import ClassSubject from '@/models/ClassSubject';
+// import Class from '../../../../../models/Class';
+// import AverageClassGrade from '../../../../../models/AverageClassGrade';
 
-const handler = async (req, res) => {
-  const session = await getServerSession(req, res, authOptions);
+// const handler = async (req, res) => {
+//   const session = await getServerSession(req, res, authOptions);
 
-  if (session) {
-    if (session.user.isAdmin === 2 || session.user.isAdmin === 0) {
-      return res.status(401).send('Unauthorized Access');
-    }
-  } else {
-    return res.status(401).send('Signin required');
-  }
+//   if (session) {
+//     if (session.user.isAdmin === 2 || session.user.isAdmin === 0) {
+//       return res.status(401).send('Unauthorized Access');
+//     }
+//   } else {
+//     return res.status(401).send('Signin required');
+//   }
 
-  const teacher_id = session.user._id;
+//   const teacher_id = session.user._id;
 
-  const years = Array.from({ length: 5 }, (_, i) => req.query.year - i);
+//   const years = Array.from({ length: 5 }, (_, i) => req.query.year - i);
 
-  await db.connect();
-  const classBatch = await Promise.all(
-    years.map(async (year) => {
-      const classBatchItem = await Class.find({ batch: year }).select('batch');
-      return [year, ...classBatchItem.map((item) => item._id.toString())];
-    })
-  );
+//   await db.connect();
+//   const classBatch = await Promise.all(
+//     years.map(async (year) => {
+//       const classBatchItem = await Class.find({ batch: year }).select('batch');
+//       return [year, ...classBatchItem.map((item) => item._id.toString())];
+//     })
+//   );
 
-  const transformedClassBatch = classBatch.reduce((acc, item) => {
-    acc[item[0]] = item.slice(1);
-    return acc;
-  }, {});
+//   const transformedClassBatch = classBatch.reduce((acc, item) => {
+//     acc[item[0]] = item.slice(1);
+//     return acc;
+//   }, {});
 
-  const columnData = await Promise.all(
-    Object.entries(transformedClassBatch).map(async ([year, classIds]) => {
-      let passedCount = 0;
-      let failedCount = 0;
+//   const columnData = await Promise.all(
+//     Object.entries(transformedClassBatch).map(async ([year, classIds]) => {
+//       let passedCount = 0;
+//       let failedCount = 0;
 
-      for (const classId of classIds) {
-        const classSubjectList = await ClassSubject.find({
-          teacher_id,
-          class_id: classId,
-          isGradeFinalized: true,
-        });
+//       for (const classId of classIds) {
+//         const classSubjectList = await ClassSubject.find({
+//           teacher_id,
+//           class_id: classId,
+//           isGradeFinalized: true,
+//         });
 
-        if (classSubjectList.length === 0) {
-          continue;
-        }
+//         if (classSubjectList.length === 0) {
+//           continue;
+//         }
 
-        for (const clsSubjectItem of classSubjectList) {
-          const averageClassSubjectGrade = await AverageClassGrade.findOne({
-            classSubject_id: clsSubjectItem._id,
-          }).select('passed failed');
+//         for (const clsSubjectItem of classSubjectList) {
+//           const averageClassSubjectGrade = await AverageClassGrade.findOne({
+//             classSubject_id: clsSubjectItem._id,
+//           }).select('passed failed');
 
-          passedCount += averageClassSubjectGrade.passed || 0;
-          failedCount += averageClassSubjectGrade.failed || 0;
-        }
-      }
+//           passedCount += averageClassSubjectGrade.passed || 0;
+//           failedCount += averageClassSubjectGrade.failed || 0;
+//         }
+//       }
 
-      return { year, passed: passedCount, failed: failedCount };
-    })
-  );
+//       return { year, passed: passedCount, failed: failedCount };
+//     })
+//   );
 
-  const passedValues = columnData.map((item) => item.passed);
-  const failedValues = columnData.map((item) => item.failed);
+//   const passedValues = columnData.map((item) => item.passed);
+//   const failedValues = columnData.map((item) => item.failed);
 
-  const highestValue = Math.max(...passedValues, ...failedValues);
+//   const highestValue = Math.max(...passedValues, ...failedValues);
 
-  const range = highestValue - 0;
+//   const range = highestValue - 0;
 
-  const interval = Math.ceil(range / 5);
-  const increment = Math.ceil(interval / 2);
+//   const interval = Math.ceil(range / 5);
+//   const increment = Math.ceil(interval / 2);
 
-  await db.disconnect();
+//   await db.disconnect();
 
-  res.status(200).json({
-    passedFailed: columnData,
-    highestValue: highestValue + increment,
-    interval,
-  });
-};
+//   res.status(200).json({
+//     passedFailed: columnData,
+//     highestValue: highestValue + increment,
+//     interval,
+//   });
+// };
 
-export default handler;
+// export default handler;
--- pages/faculty/home.js
+++ pages/faculty/home.js
@@ -1,62 +1,62 @@
 import FacultyLayout from '@/components/faculty/FacultyLayout';
 import Card from '@/components/admin/Card';
 
-import StudentsbyYear from '../../components/faculty/charts/StudentsbyYear';
-import { useQuery } from '@tanstack/react-query';
-import {
-  fetchAverageClassGradeYearly,
-  fetchPassedFailedStudent,
-  // fetchPassedFailedStudent,
-  fetchStudentsByYearLevel,
-} from '../../components/hooks/FacultySubject/fetch';
-import AveragePerformance from '../../components/faculty/charts/AveragePerformance';
-import PassedFailed from '../../components/faculty/charts/PassedFailed';
-import { useMemo } from 'react';
+// import StudentsbyYear from '../../components/faculty/charts/StudentsbyYear';
+// import { useQuery } from '@tanstack/react-query';
+// import {
+//   fetchAverageClassGradeYearly,
+//   fetchPassedFailedStudent,
+//   // fetchPassedFailedStudent,
+//   fetchStudentsByYearLevel,
+// } from '../../components/hooks/FacultySubject/fetch';
+// import AveragePerformance from '../../components/faculty/charts/AveragePerformance';
+// import PassedFailed from '../../components/faculty/charts/PassedFailed';
+// import { useMemo } from 'react';
 
 function HomeScreen() {
-  const currentDate = useMemo(() => new Date(), []);
-  const currentYear = useMemo(() => currentDate.getFullYear(), [currentDate]);
-  const currentMonth = useMemo(() => currentDate.getMonth() + 1, [currentDate]);
+  // const currentDate = useMemo(() => new Date(), []);
+  // const currentYear = useMemo(() => currentDate.getFullYear(), [currentDate]);
+  // const currentMonth = useMemo(() => currentDate.getMonth() + 1, [currentDate]);
 
-  // Fetch the student data by year level
-  const studentQuery = useQuery(
-    ['studentCurrentYearLevelSemester'],
-    () => fetchStudentsByYearLevel(currentYear, currentMonth),
-    {
-      refetchOnMount: false, // Avoid refetching on component mount
-      refetchOnWindowFocus: false,
-    }
-  );
+  // // Fetch the student data by year level
+  // const studentQuery = useQuery(
+  //   ['studentCurrentYearLevelSemester'],
+  //   () => fetchStudentsByYearLevel(currentYear, currentMonth),
+  //   {
+  //     refetchOnMount: false, // Avoid refetching on component mount
+  //     refetchOnWindowFocus: false,
+  //   }
+  // );
 
-  // Fetch the passed/failed student data
-  const passedFailedQuery = useQuery(
-    ['passedFailedStudentYearly'],
-    () => fetchPassedFailedStudent(currentYear),
-    {
-      enabled: studentQuery.isSuccess, // Enable the query only when the studentQuery has succeeded
-      refetchOnMount: false, // Avoid refetching on component mount
-      refetchOnWindowFocus: false,
-    }
-  );
+  // // Fetch the passed/failed student data
+  // const passedFailedQuery = useQuery(
+  //   ['passedFailedStudentYearly'],
+  //   () => fetchPassedFailedStudent(currentYear),
+  //   {
+  //     enabled: studentQuery.isSuccess, // Enable the query only when the studentQuery has succeeded
+  //     refetchOnMount: false, // Avoid refetching on component mount
+  //     refetchOnWindowFocus: false,
+  //   }
+  // );
 
-  // Fetch the average class grade data
-  const averageClassGradeQuery = useQuery(
-    ['averageClassGradeYearly'],
-    () => fetchAverageClassGradeYearly(currentYear),
-    {
-      enabled: passedFailedQuery.isSuccess, // Enable the query only when the passedFailedQuery has succeeded
-      refetchOnMount: false, // Avoid refetching on component mount
-      refetchOnWindowFocus: false,
-    }
-  );
+  // // Fetch the average class grade data
+  // const averageClassGradeQuery = useQuery(
+  //   ['averageClassGradeYearly'],
+  //   () => fetchAverageClassGradeYearly(currentYear),
+  //   {
+  //     enabled: passedFailedQuery.isSuccess, // Enable the query only when the passedFailedQuery has succeeded
+  //     refetchOnMount: false, // Avoid refetching on component mount
+  //     refetchOnWindowFocus: false,
+  //   }
+  // );
 
-  if (
-    studentQuery.isLoading ||
-    averageClassGradeQuery.isLoading ||
-    passedFailedQuery.isLoading
-  ) {
-    return null;
-  }
+  // if (
+  //   studentQuery.isLoading ||
+  //   averageClassGradeQuery.isLoading ||
+  //   passedFailedQuery.isLoading
+  // ) {
+  //   return null;
+  // }
 
   return (
     <FacultyLayout title="Home">
@@ -68,7 +68,7 @@ function HomeScreen() {
           <Card title={'Earnings'} value={25} words={'Wew'} />
         </div>
 
-        <div className="grid grid-cols-12 grid-rows-12 gap-3 mt-3">
+        {/* <div className="grid grid-cols-12 grid-rows-12 gap-3 mt-3">
           <div className="col-span-5 row-span-5 row-start-1  rounded-xl p-8 bg-white">
             <h3 className="font-semibold text-h6 mb-3">
               Students by Year Level
@@ -119,7 +119,7 @@ function HomeScreen() {
               />
             )}
           </div>
-        </div>
+        </div>*/}
       </div>
     </FacultyLayout>
   );
